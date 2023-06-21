c34d676508aec3e55bb246218b7468caca9054c4
Mariia Onufriievych <onufrievich.maria@gmail.com>
2023-06-21 18:17:13 +0200

    some stuff

---
+++ frontend/src/components/Layout/Layout.vue
@@ -0,0 +1,38 @@
+<script setup>
+import  Navbar  from '../Navbar/Navbar.vue';
+import  TabContent  from '../TabContent/TabContent.vue';
+
+</script>
+
+<template>
+    <div class="layout">
+        <div class="left">
+            <Navbar/>
+    </div>
+    <div class="right">
+        <TabContent/>
+      
+    </div>
+
+    </div>
+    
+  </template>
+  
+  <style>
+  
+
+    .layout {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+    .left {
+        width: 300px;
+    }
+    .right {
+        width: 100%;
+    }
+    
+  
+  </style>
+  
---
+++ frontend/src/components/Navbar/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
+
+
